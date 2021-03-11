df08053bdeb9fdb4a95bfd799e6ba8194180f6ea
Andrew Kump <kumpandrew@gmail.com>
2021-03-10 23:54:20 -0500

    use single quotes

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ import gulp from 'gulp'
 import zip from 'gulp-zip'
 
 export default () => (
-    gulp.src(['manifest.json', 'dist/**', 'images/**'], { "base": "." })
+    gulp.src(['manifest.json', 'dist/**', 'images/**'], { 'base': '.' })
         .pipe(zip('extension.zip'))
         .pipe(gulp.dest('production'))
 );
